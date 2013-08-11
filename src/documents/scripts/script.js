b4160ab90c282e7717436411e23ed521b9ecb38e
Michael Mooring <mikeumus@gmail.com>
2013-08-11 02:10:27 -0400

    updates.

--- src/documents/scripts/script.js
+++ src/documents/scripts/script.js
@@ -1,6 +1,9 @@
-YUI().use('node','event',
+YUI({
+        gallery: 'gallery-2013.03.27-22-06'
+    }).use('gallery-tipsy','gallery-popover','node','event',
 function(Y)
 {
+	// homepage section background events
 	Y.one('.splash-publisher').on({
 		mouseover: function(){
 			Y.one('#splash-img-1').setStyle('opacity',1);
@@ -17,4 +20,11 @@ function(Y)
 			Y.one('#splash-img-2').setStyle('opacity',0.44);
 		}
 	});
+
+     var tipsy = new Y.Tipsy({
+        selector: "[rel='tipsy']"
+    });
+
+    tipsy.render(); 
+
 });
